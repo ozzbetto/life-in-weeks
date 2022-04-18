b10e2bd1ca8e84c330b60b73e337790d2026c3fa
Betto Rodriguez <segovia.al@gmail.com>
2022-04-17 20:05:20 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+age = input("What is your current age?")
+
+new_age = 90 - int(age)
+in_days = str((new_age) * 365)
+# print(in_days)
+
+in_weeks = str((new_age) * 52)
+#print(in_weeks)
+
+in_months = str((new_age) * 12)
+#print(in_months)
+
+life = print(f"You have {in_days} days, {in_weeks} weeks, and {in_months} months left" )
